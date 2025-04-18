c7827bbec8f45ae1bc7a646b49c29e6fa4efcf34
MuliJob <munyoki912@gmail.com>
2025-04-18 22:17:09 +0300

    updated urls

--- example/urls.py
+++ example/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView.as_view()),
-    path("about", views.AboutView.as_view()),
+    path("", views.HomeView.as_view(), name="home"),
+    path("about", views.AboutView.as_view(), name="about"),
 ]
