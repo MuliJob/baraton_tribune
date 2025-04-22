a2fd1b248b87166816047b5230f418028527d0a3
MuliJob <munyoki912@gmail.com>
2025-04-22 22:00:25 +0300

    Replaced Template views classes with API views classes

--- example/views.py
+++ example/views.py
@@ -1,27 +1,70 @@
 """Application views"""
-from django.views.generic import TemplateView
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class HomeView(TemplateView):
-    """Home page"""
-    template_name = "index.html"
+class HomeView(APIView):
+    """API view for home"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
 
-class CategoryView(TemplateView):
-    """Category Page"""
-    template_name = "category.html"
+    def get(self, request, *args, **kwargs):
+        """Get method"""
 
-class PostDetail(TemplateView):
-    """Post Detail Page"""
-    template_name = "post-details.html"
+        return Response({}, template_name=self.template_name)
 
-class AuthorDetail(TemplateView):
-    """Author Detail Page"""
-    template_name = "author-profile.html"
 
-class AboutView(TemplateView):
-    """About page"""
-    template_name = "about.html"
+class CategoryView(APIView):
+    """API view for categories"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'category.html'
 
-class ContactView(TemplateView):
-    """Contact page"""
-    template_name = "contact.html"
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+
+        return Response({}, template_name=self.template_name)
+
+
+class PostDetail(APIView):
+    """API view for post details"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'post-details.html'
+
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+
+        return Response({}, template_name=self.template_name)
+
+
+class AuthorDetail(APIView):
+    """API view for author details"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'author-profile.html'
+
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+
+        return Response({}, template_name=self.template_name)
+
+
+class AboutView(APIView):
+    """API view for about"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'about.html'
+
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+
+        return Response({}, template_name=self.template_name)
+
+
+class ContactView(APIView):
+    """API view for contact"""
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'contact.html'
+
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+
+        return Response({}, template_name=self.template_name)
