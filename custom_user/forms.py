7e59dcbd148176b8823c79a1a789db26d5480c27
MuliJob <munyoki912@gmail.com>
2025-05-01 21:50:39 +0300

    Created manual input fields for login and register

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,6 +1,7 @@
 """Login and register form"""
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
+from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
 
 from .models import User
@@ -11,23 +12,13 @@ from .models import User
 class CreateUserForm(UserCreationForm):
     """Register form"""
     email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={'class': 'form-control', 'placeholder': 'Enter your email'}
-        )
+
     )
 
     password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(
-            attrs={'class': 'form-control', 'placeholder': 'Enter your password'}
-        )
     )
 
     password2 = forms.CharField(
-        label="Confirm Password",
-        widget=forms.PasswordInput(
-            attrs={'class': 'form-control', 'placeholder': 'Confirm your password'}
-        )
     )
 
     class Meta:
@@ -48,12 +39,8 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     """Login form (Customized)"""
     username = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={'class': 'form-control', 'placeholder': 'Enter your email'}
-        )
+
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={'class': 'form-control', 'placeholder': 'Enter your password'}
-        )
+
     )
