398febc61df454e85c0cf3e4b484f1a7732037e2
MuliJob <munyoki912@gmail.com>
2025-05-01 22:11:58 +0300

    Cleaning data for user login for cases where wrong password and wrong email

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -44,3 +45,21 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(
 
     )
+
+    def clean(self):
+        email = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            if not User.objects.filter(email=email).exists():
+                self.add_error('username', _("No account found with this email address."))
+                return self.cleaned_data
+
+            user = authenticate(self.request, username=email, password=password)
+
+            if user is None:
+                self.add_error('password', _("Incorrect password. Please try again!"))
+            elif not user.is_active:
+                self.add_error('username', _("This account is inactive."))
+
+        return self.cleaned_data
