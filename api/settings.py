d9123919fd34d11f940c510d4573ff573da77f16
MuliJob <munyoki912@gmail.com>
2025-05-02 14:30:31 +0300

    Getting allowed hosts from env file

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
+
 
 
 # Application definition
