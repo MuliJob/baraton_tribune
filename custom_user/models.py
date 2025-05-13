ce0c58bd173a417a331e27bda6ce27866db40a70
MuliJob <munyoki912@gmail.com>
2025-05-13 23:21:25 +0300

    Created models.py file for user profile details

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,7 +1,12 @@
 """"Models for custom user app."""
 from django_use_email_as_username.models import BaseUser, BaseUserManager
+from django.db import models
 
 
 class User(BaseUser):
     """Custom user model that uses email as username."""
     objects = BaseUserManager()
+
+
+class UserProfile(models.Model):
+    """User profile"""
